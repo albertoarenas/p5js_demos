5a5419f97892f095106847e3ad39100a056229cc
alberto.arenas.garcia <alberto.arenas.garcia@gmail.com>
2021-03-24 01:54:29 +0100

    feature. added peak detection for different freq

--- graphAmp/sketch_graphamp.js
+++ graphAmp/sketch_graphamp.js
@@ -1,13 +1,27 @@
 
 let song;
+
 let amp;
+let fft;
+let peakLow;
+let peakMid;
+let peakHigh;
+
+var onsetLow,beatLow;
+
 let buttonPlay;
 let sliderVolume;
+
 let volHistory = [];
+let peakLowHistory = [];
+let peakMidHistory = [];
+let peakHighHistory = [];
+let beatLowHistory = [];
+let onsetLowHistory = [];
 
 
 function setup() {
-	createCanvas(300, 300)
+	createCanvas(600, 300)
 
 	let song1File = "audio/Digi GAlessio - darix togni.mp3";
 	let song2File = "audio/Monkey Warhol - Lunar Phases.mp3";
@@ -22,7 +36,7 @@ function loaded(){
 	buttonPlay.mousePressed(togglePlaying);
 
 	// Create volume slider
-	sliderVolume = createSlider(0, 1, 0.1, 0.01);
+	sliderVolume = createSlider(0, 1, 0.5, 0.01);
 
 	console.log("Song loaded");
 }
@@ -30,14 +44,28 @@ function loaded(){
 function togglePlaying(){
 	if (!song.isPlaying()){
 		song.play();
+		
 		// Amplitude
 		amp = new p5.Amplitude();
 		amp.setInput(song);
-		buttonPlay.html("Pause")
+		
+		fft = new p5.FFT();
+		fft.setInput(song);
+
+		peakLow = new p5.PeakDetect(40,120,threshold=0.5);
+		peakMid = new p5.PeakDetect(5000,10000,threshold=0.5);
+		peakHigh = new p5.PeakDetect(10000,20000,threshold=0.5);
+
+		onsetLow = new OnsetDetect(40,120,"bass",0.025);
+   		beatLow = new BeatDetect(40,120,"bass",0.95);
+		
+		buttonPlay.html("Stop")
 	}
 	else{
 		song.stop();
 		amp = null;
+		fft = null;
+		peak = null;
 		buttonPlay.html("Play")
 	}
 }
@@ -48,9 +76,49 @@ function draw() {
 
 	if (song.isPlaying()){
 		song.setVolume(sliderVolume.value());
+
 		let vol = amp.getLevel();
 		let y = map(vol, 0, 1, height, 0);
 		volHistory.push(y);
+
+		fft.analyze();
+
+		peakLow.update(fft);
+		if (peakLow.isDetected) {
+			peakLowHistory.push(1);
+		} else {
+			peakLowHistory.push(0);
+		}
+
+		peakMid.update(fft);
+		if (peakMid.isDetected) {
+			peakMidHistory.push(1);
+		} else {
+			peakMidHistory.push(0);
+		}
+
+		peakHigh.update(fft);
+		if (peakHigh.isDetected) {
+			peakHighHistory.push(1);
+		} else {
+			peakHighHistory.push(0);
+		}
+
+		// onsetLow.update(fft);
+		// if (onsetLow.isDetected) {
+		// 	onsetLowHistory.push(1);
+		// } else {
+		// 	onsetLowHistory.push(0);
+		// }
+
+		// beatLow.update(fft);
+		// if (beatLow.isDetected) {
+		// 	console.log('beat');
+		// 	beatLowHistory.push(1);
+		// } else {
+		// 	beatLowHistory.push(0);
+		// 	console.log('no beat');
+		// }
 
 		push();
 		stroke(255);
@@ -58,21 +126,146 @@ function draw() {
 		noFill();
 		translate(0, -height/2)
 		for(var i = 0; i < volHistory.length; i++) {
-			
+
+			stroke(255, 255, 255);
 			vertex(i, volHistory[i]);
 			//console.log(i, y)
+
+			if(peakLowHistory[i] == 1) {
+				stroke(255, 0, 0);
+				line(i, 0, i, height)
+			}
+
+			if(peakMidHistory[i] == 1) {
+				stroke(0, 255, 0);
+				line(i, 0, i, height)
+			}
+
+			if(peakHighHistory[i] == 1) {
+				stroke(100, 100, 255);
+				line(i, 0, i, height)
+			}
+
+			// if(beatLowHistory[i] == 1) {
+			// 	stroke(255, 0, 255);
+			// 	line(i, 0, i, height)
+			// }
+
+			// if(onsetLowHistory[i] == 1) {
+			// 	stroke(255, 0, 255);
+			// 	line(i, 0, i, height)
+			// }
 		}
 		endShape();
 
 		if (volHistory.length > width-50) {
 			volHistory.splice(0, 1);
+			peakLowHistory.splice(0, 1);
+			peakMidHistory.splice(0, 1);
+			peakHighHistory.splice(0, 1);
+			//beatLowHistory.splice(0, 1);
 		}
 		pop();
+
 		stroke(255, 0, 0);
 		line(volHistory.length, 0, volHistory.length, height)
 	}
 	else{
 		volHistory = [];
+		peakLowHistory = [];
+		peakMidHistory = [];
+		peakHighHistory = [];
+		onsetLowHistory = [];
+		beatLow = [];
 	}
 	//console.log(vol)
+}
+
+
+function OnsetDetect(f1,f2,str,thresh){
+    this.isDetected = false;
+    this.f1=f1;
+    this.f2=f2;
+    this.str = str;
+    this.treshold = thresh;
+    this.energy = 0;
+    this.penergy =0;
+    this.siz = 10;
+    this.sensitivity = 10;
+}
+
+OnsetDetect.prototype.display = function(x,y) {
+
+    if(this.isDetected == true){
+        this.siz = lerp(this.siz,40,0.99);
+    }
+    else if (this.isDetected == false){
+        this.siz = lerp(this.siz,15,0.99);
+    }
+    fill(255,0,0);
+    ellipse(x,y,this.siz,this.siz);
+    fill(0);
+    text(this.str,x,y);
+    text("( "+this.f1+" - "+this.f2+"Hz )",x,y+10);
+}
+
+OnsetDetect.prototype.update = function(fftObject) {
+    this.energy = fftObject.getEnergy(this.f1,this.f2)/255;
+
+    if(this.isDetected == false){
+        if (this.energy-this.penergy > this.treshold){
+            this.isDetected = true;
+            var self = this;
+            setTimeout(function () {
+                self.isDetected = false;
+            },this.sensitivity);
+        }
+    }
+
+    this.penergy = this.energy;
+
+}
+
+
+function BeatDetect(f1,f2,str,thresh){
+    this.isDetected = false;
+    this.f1=f1;
+    this.f2=f2;
+    this.str = str;
+    this.treshold = thresh;
+    this.energy = 0;
+
+    this.siz = 10;
+    this.sensitivity = 500;
+}
+
+BeatDetect.prototype.display = function(x,y) {
+
+    if(this.isDetected == true){
+        this.siz = lerp(this.siz,40,0.99);
+    }
+    else if (this.isDetected == false){
+        this.siz = lerp(this.siz,15,0.99);
+    }
+    fill(255,0,0);
+    ellipse(x,y,this.siz,this.siz);
+    fill(0);
+    text(this.str,x,y);
+    text("( "+this.f1+" - "+this.f2+"Hz )",x,y+10);
+}
+
+BeatDetect.prototype.update = function(fftObject) {
+    this.energy = fftObject.getEnergy(this.f1,this.f2)/255;
+
+	console.log(this.energy)
+
+    if(this.isDetected == false){
+        if (this.energy > this.treshold){
+            this.isDetected = true;
+            var self = this;
+            setTimeout(function () {
+                self.isDetected = false;
+            },this.sensitivity);
+        }
+    }   
 }
